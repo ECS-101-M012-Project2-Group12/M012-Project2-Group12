444857e6f2e21bcc782c745129f0859da39f301d
ochi7715 <92065046+ochi7715@users.noreply.github.com>
2021-11-10 16:00:41 -0500

    Add files via upload

--- Project2.py
+++ Project2.py
@@ -10,57 +10,53 @@ C2mean = 7
 C2std = 5
 C3mean = 11
 C3std = 7
- 
+
 def exploitOnly():
-    mylist = [random.normalvariate(C1mean, C1std), random.normalvariate(C2mean, C2std),
-              random.normalvariate(C3mean, C3std)]
-    if max(mylist) == mylist[0]:
-        mylist.extend([random.normalvariate(C1mean, C1std) for i in range(297)])
-    elif max(mylist) == mylist[1]:
-        mylist.extend([random.normalvariate(C2mean, C2std) for i in range(297)])
-    elif max(mylist) == mylist[2]:
-        mylist.extend([random.normalvariate(C3mean, C3std) for i in range(297)])
-    # return list
-    return sum(mylist, 0)
+    list = [random.normalvariate(C1mean, C1std), random.normalvariate(C2mean, C2std), random.normalvariate(C3mean, C3std)]
+    if max(list) == list[0]:
+        list.extend([random.normalvariate(C1mean,C1std) for i in range(297)])
+    elif max(list) == list[1]:
+        list.extend([random.normalvariate(C2mean,C2std) for i in range(297)])
+    else:
+        list.extend([random.normalvariate(C3mean,C3std) for i in range(297)])
+    return sum(list,0)
 
-
-print(exploitOnly())
+#print(exploitOnly())
 #print(len(exploitOnly()))
 
 def exploreOnly():
-    happiness1 = [random.normalvariate(C1mean, C1std) for i in range(100)]
-    happiness2 = [random.normalvariate(C2mean, C2std) for i in range(100)]
-    happiness3 = [random.normalvariate(C3mean, C3std) for i in range(100)]
-    joinedlists = happiness1 + happiness2 + happiness3
+    list = [random.normalvariate(C1mean,C1std) for i in range(100)]
+    list.extend([random.normalvariate(C2mean,C2std) for i in range(100)])
+    list.extend([random.normalvariate(C3mean,C3std) for i in range(100)])
     #return list
-    return sum(joinedlists)
+    return sum(list,0)
 
-print(exploreOnly())
+#print(exploreOnly())
 
 def eGreedy(p1:int):
     list = [random.normalvariate(C1mean, C1std), random.normalvariate(C2mean, C2std), random.normalvariate(C3mean, C3std)]
     if max(list) == list[0]:
-        for i in range(300):
+        for i in range(297):
             if random.normalvariate(0,300) > 3*p1:
                 list.extend([random.normalvariate(C1mean,C1std)])
             else:
                 list.extend([random.choice([random.normalvariate(C1mean,C1std),random.normalvariate(C2mean,C2std),random.normalvariate(C3mean,C3std)])])
     elif max(list) == list[1]:
-        for i in range(300):
+        for i in range(297):
             if random.normalvariate(0,300) > 3*p1:
                 list.extend([random.normalvariate(C2mean,C2std)])
             else:
                 list.extend([random.choice([random.normalvariate(C1mean,C1std),random.normalvariate(C2mean,C2std),random.normalvariate(C3mean,C3std)])])
-    elif max(list) == list[2]:
-        for i in range(300):
+    else:
+        for i in range(297):
             if random.normalvariate(0,300) > 3*p1:
                 list.extend([random.normalvariate(C3mean,C3std)])
             else:
                 list.extend([random.choice([random.normalvariate(C1mean,C1std),random.normalvariate(C2mean,C2std),random.normalvariate(C3mean,C3std)])])
-    #return list
-    return sum(list,0)
+    return sum(list)
+    #return sum(list,0)
 
-print(eGreedy(2))
+#print(eGreedy(12))
 
 def Simulation(trials:int, x:int) -> str:
     exploitlist = [exploitOnly() for i in range(trials)]
@@ -70,8 +66,38 @@ def Simulation(trials:int, x:int) -> str:
     #print(explorelist)
     #print(greedylist)
     return("Exploit Best: {exploitbest} \nExploit Mean: {exploitmean} \nExploit Regret: {exploitregret} \nExplore Best: {explorebest} \nExplore Mean: {exploremean} \nExplore Regret: {exploreregret} \nGreedy Best: {greedybest} \nGreedy Mean: {greedymean} \nGreedy Regret: {greedyregret}"\
-        .format(exploitbest = (42+(18*297)), exploitmean = statistics.mean(exploitlist), exploitregret = (42+(18*297)-statistics.mean(exploitlist)), \
-            explorebest = (42*100), exploremean = statistics.mean(explorelist),exploreregret = (4200-statistics.mean(explorelist)), \
-                greedybest = (18*300), greedymean = statistics.mean(greedylist),greedyregret = (18*300)-statistics.mean(greedylist)))
+        .format(exploitbest = (42+(18*297)), exploitmean = statistics.mean(exploitlist), exploitregret = (3300-statistics.mean(exploitlist)), \
+            explorebest = (42*100), exploremean = statistics.mean(explorelist),exploreregret = (3300-statistics.mean(explorelist)), \
+                greedybest = (18*300), greedymean = statistics.mean(greedylist),greedyregret = (3300-statistics.mean(greedylist))))
 
-print(Simulation(200,4))
+print(Simulation(100,12))
+print("-----------------------------------------------------" )
+print(Simulation(10000,12))
+print("-----------------------------------------------------" )
+print(Simulation(100000,12))
+
+def findbestegreedy():
+    index = 0
+    emax = 0
+    for i in range(0,101):
+        a = eGreedy(i)
+        if a > emax:
+            emax = a
+            index = i
+    return index
+    # + " " + str(list.index(max(list))
+def bestxpercent(x:int):
+    list = []
+    for i in range(x):
+        list.append(findbestegreedy())
+    a = statistics.mode(list)
+    return a
+
+def bestpercent(x:int):
+    list = []
+    for i in range(x):
+        list.append(bestxpercent(x))
+    return ("Mean: {meana}\nStandard Deviation: {stdev}".format(meana = statistics.mean(list), stdev = statistics.stdev(list)))
+
+print("-----------------------------------------------------" )
+#print(bestpercent(10))
